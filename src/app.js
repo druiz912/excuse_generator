b0124058f42640ec90579a5295c2be06d69766bd
druiz912 <druiz264@gmail.com>
2022-07-19 18:10:41 +0000

    fix error with the reload function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = [
+    "Mi perro",
+    "David Bisbal",
+    "Spider-Man",
+    "Manolo Escobar",
+    "Winston Churchill"
+  ];
+  const what = [
+    "se meó",
+    "me ató a un contenedor",
+    "se clavó un pelo en el ojo",
+    "contrató a 20 guardaespaldas",
+    "me arrestó"
+  ];
+  const where = [
+    "en mi casa",
+    "en Marbella",
+    "en la playa de Matalascañas",
+    "donde iba Messi para entrenar",
+    "en Júpiter",
+    "en el bar de abajo",
+    "en Mordor."
+  ];
+
+  let sentence =
+    who[numAle(who.length)] +
+    " " +
+    what[numAle(what.length)] +
+    " " +
+    where[numAle(where.length)];
+
+  let excuse = document.querySelector("#excuse");
+  excuse.innerHTML = sentence;
 };
+
+function numAle(length) {
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
